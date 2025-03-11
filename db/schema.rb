d9328ace66df4e3b74e02a0ab06f45a4867e1e3c
Denise Pena <Denisepena312@gmail.com>
2025-03-11 16:02:57 +0100

    matches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_11_103120) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_11_145027) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_11_103120) do
     t.string "lat"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "matches", force: :cascade do |t|
@@ -82,6 +83,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_11_103120) do
     t.integer "rate"
     t.string "short_description"
     t.boolean "guide"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

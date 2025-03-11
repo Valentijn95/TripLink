22cee7135c9a31ef35c9e2281e8dbfda18ed6035
Denise Pena <Denisepena312@gmail.com>
2025-03-11 15:20:25 +0100

    committtt

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,10 +1,22 @@
 class MatchesController < ApplicationController
-  before_action :set_match, only: [:show, :chat]
+  before_action :set_match, only: [:show, :chat, :send_message]
 
   def show
+    # You can test if @match is set correctly
   end
 
   def chat
+    # Chat-related logic
+  end
+
+  def send_message
+    @message = @match.messages.build(content: params[:content], user: current_user)
+
+    if @message.save
+      redirect_to match_path(@match)
+    else
+      render :chat
+    end
   end
 
   private
@@ -13,4 +25,3 @@ class MatchesController < ApplicationController
     @match = Match.find(params[:id])
   end
 end
-

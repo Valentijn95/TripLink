de4b06b4e09ca64af32f36baf7292d6ebc428256
Timo Vromen <timovromen@gmail.com>
2025-03-12 13:05:59 +0100

    cloudinary ready with update user with photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   helper_method :no_footer_path
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def no_footer_path
     [:root]
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
   end
 end

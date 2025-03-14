78fac0d52516fd0b6e563d87d4f53d410f4b17f7
Denise Pena <Denisepena312@gmail.com>
2025-03-14 11:21:22 +0100

    Update schema.rb with latest migration changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_13_212815) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_13_164304) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -103,6 +103,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_13_212815) do
     t.text "payload"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
     t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
   end
 

05590ceffcaff4735ac42ab684dbc7bef39c1e94
Valentijn Weinans <valentijn@smarttrackers.nl>
2025-03-15 12:25:45 +0100

    production active storage set to cloudinary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.active_storage.service = :cloudinary
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
@@ -38,8 +37,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
+  # config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"

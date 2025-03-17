82ba306403ac702974927782901cf3c167d0f756
Denise Pena <Denisepena312@gmail.com>
2025-03-17 11:19:20 +0100

    first message

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,27 +1,43 @@
 class MatchesController < ApplicationController
-  before_action :set_match, only: [:show]
+  before_action :set_guide, only: [:new]
+  before_action :set_match, only: [:show, :create_message]
 
   def show
-    if current_user == @match.guide
-      @matched_user = @match.tourist
-    else
-      @matched_user = @match.guide
-    end
+    @match = Match.find(params[:id])
     @message = Message.new
+    @matched_user = @match.guide
   end
 
   def new
     @guide = User.find(params[:guide_id])
-    @match = Match.new
+    @match = Match.find_or_create_by(guide_id: @guide.id, tourist_id: current_user.id)
+    @message = Message.new
   end
 
-  def create
-    raise
+  def create_message
+    @match = Match.find(params[:match_id])
+    @message = Message.new(message_params)
+    @message.user = current_user
+    @message.match = @match
+
+    if @message.save
+      redirect_to match_path(@match), notice: 'Message sent successfully!'
+    else
+      render :new
+    end
   end
 
   private
 
+  def set_guide
+    @guide = User.find(params[:guide_id])
+  end
+
   def set_match
-    @match = Match.find(params[:id])
+    @match = Match.find(params[:match_id] || params[:id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show]
 
-   get 'matches/new/:guide_id', to: 'matches#new', as: 'new_match'
+  get 'matches/new/:guide_id', to: 'matches#new', as: 'new_match'
+  post 'matches/new/:guide_id', to: 'matches#create_message', as: 'create_message'
 
   resources :matches, only: [:index, :show, :new, :create] do
     resources :messages, only: [:create]
@@ -19,5 +20,5 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
 end
+

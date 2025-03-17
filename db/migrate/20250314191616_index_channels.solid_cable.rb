f494641bb366d07ff5fb03059f1882deb42835a7
Denise Pena <Denisepena312@gmail.com>
2025-03-17 14:45:08 +0100

    style

--- db/migrate/20250314191616_index_channels.solid_cable.rb
+++ db/migrate/20250314191616_index_channels.solid_cable.rb
@@ -3,6 +3,8 @@
 # This migration comes from solid_cable (originally 20240607184711)
 class IndexChannels < ActiveRecord::Migration[7.1]
   def change
-    add_index :solid_cable_messages, :channel, length: 500
+    unless index_exists?(:solid_cable_messages, :channel)
+      add_index :solid_cable_messages, :channel, length: 500
+    end
   end
 end
